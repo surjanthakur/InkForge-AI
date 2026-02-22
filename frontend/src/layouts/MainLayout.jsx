82f90df577a113573584110570fd449238092cf6
surjanthakur <tsurjan506@gmail.com>
2026-02-22 22:03:57 +0530

    save

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,9 +1,19 @@
-import {Outlet} from "react-router-dom"
+import { Outlet } from 'react-router-dom';
+import { Navbar, Footer } from '../components/index';
 
 const MainLayout = () => {
-    return (
-        <Outlet/>
-    );
-}
+  return (
+    <div className="min-h-screen flex flex-col bg-black">
+      {/* header */}
+      <Navbar />
+      {/* main content */}
+      <main className="grow">
+        <Outlet />
+      </main>
+      {/* footer */}
+      <Footer />
+    </div>
+  );
+};
 
 export default MainLayout;
