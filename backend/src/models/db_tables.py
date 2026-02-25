0f1df6ad31662d4b7c24af6d43ed4776340e9847
surjanthakur <tsurjan506@gmail.com>
2026-02-26 01:19:16 +0530

    save

--- backend/src/models/db_tables.py
+++ backend/src/models/db_tables.py
@@ -16,10 +16,10 @@ class User(SQLModel, table=True):
     """
 
     user_id: UUID = Field(
-        default_factory=lambda: uuid.uuid4(), primary_key=True, unique=True
+        default_factory=lambda: uuid.uuid4(), primary_key=True, unique=True, index=True
     )
     username: str = Field(unique=True, index=True, min_length=3, max_length=50)
-    email: str = Field(unique=True, index=True, min_length=5, max_length=255)
+    email: str = Field(unique=True, min_length=5, max_length=255)
     hashed_password: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
     posts: Optional[List["Post"]] = Relationship(back_populates="owner")
@@ -44,7 +44,7 @@ class Post(SQLModel, table=True):
     """
 
     post_id: UUID = Field(
-        default_factory=lambda: uuid.uuid4(), primary_key=True, unique=True
+        default_factory=lambda: uuid.uuid4(), primary_key=True, unique=True, index=True
     )
     user_id: UUID = Field(
         foreign_key="user.user_id",
