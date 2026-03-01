29c92894759bf57de0cdebc93fb20b7499b488c2
surjanthakur <tsurjan506@gmail.com>
2026-03-01 11:34:29 +0530

    save

--- backend/src/db/redis_client.py
+++ backend/src/db/redis_client.py
@@ -1,4 +1,3 @@
 import redis.asyncio
 
-
 redis_client = redis.asyncio.Redis(host="localhost", port=6379, decode_responses=True)
--- backend/src/service/users_service.py
+++ backend/src/service/users_service.py
@@ -73,7 +73,8 @@ async def authenticate_user(
         )
 
         return {"message": "Login successful"}
-    except Exception:
+    except Exception as e:
+        print(f"Login error: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="something went wrong try again!",
