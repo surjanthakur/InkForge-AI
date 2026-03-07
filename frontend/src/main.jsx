2bc5f88a89671f13ae2496913f35c1ff652734e5
surjanthakur <tsurjan506@gmail.com>
2026-03-07 17:51:16 +0530

    save

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthContextProvider } from "./context/authContext.js";
+import { AuthContextProvider } from "./context/authContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
