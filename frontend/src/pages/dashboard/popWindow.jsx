92a1c3047d73c7960cd96de6b2c78691a6a53e4d
surjanthakur <tsurjan506@gmail.com>
2026-03-09 19:13:45 +0530

    save

--- frontend/src/pages/dashboard/popWindow.jsx
+++ frontend/src/pages/dashboard/popWindow.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { MoreVertical } from "lucide-react";
+import { MoreVertical, ImageIcon, DownloadIcon } from "lucide-react";
 
 export default function PopupMenu() {
   const [open, setOpen] = useState(false);
@@ -30,11 +30,13 @@ export default function PopupMenu() {
         <MoreVertical size={15} />
       </button>
       {open && (
-        <div className="z-50 absolute right-0 mt-2 w-44 bg-white border border-gray-100 rounded-lg shadow-lg flex flex-col">
-          <button className="text-sm px-4 py-2 text-gray-800 hover:bg-gray-50 rounded-t-lg text-left">
+        <div className="z-50 absolute right-0 mt-2 w-max bg-white border border-gray-100 rounded-lg shadow-lg flex flex-col">
+          <button className="flex items-center gap-2 text-sm px-4 py-2 text-gray-800 hover:bg-gray-50 rounded-t-lg text-left">
+            <DownloadIcon size={16} className="text-amber-500" />
             Download as PDF
           </button>
-          <button className="text-sm px-4 py-2 text-gray-800 hover:bg-gray-50 rounded-b-lg text-left">
+          <button className="flex items-center gap-2 text-sm px-4 py-2 text-gray-800 hover:bg-gray-50 rounded-b-lg text-left">
+            <ImageIcon size={16} className="text-blue-500" />
             Download as Image
           </button>
         </div>
