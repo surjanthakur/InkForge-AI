cfed2d436f70d9a762937f16d7dc92352f5b5bae
surjanthakur <tsurjan506@gmail.com>
2026-03-17 22:31:14 +0530

    save

--- backend/src/service/ai_service.py
+++ backend/src/service/ai_service.py
@@ -11,14 +11,13 @@ groq_key = os.getenv("GROQ_API_KEY")
 
 async def ai_stream_response(user_input: str, username: str):
     client = AsyncGroq(api_key=groq_key)
+    prompt = chatbot_prompt(curr_user=username)
 
     stream = await client.chat.completions.create(
         model="openai/gpt-oss-120b",
         messages=[
-            {
-                "role": "system",
-                "content": chatbot_prompt(curr_user=username),
-            },
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": user_input},
         ],
         stream=True,
         temperature=0.95,
@@ -30,4 +29,6 @@ async def ai_stream_response(user_input: str, username: str):
         tools=[{"type": "browser_search"}],
     )
     async for chunk in stream:
-        return chunk.choices[0].delta.content
+        content = chunk.choices[0].delta.content or ""
+        if content:
+            yield content  # ✅ stream chunk-by-chunk
