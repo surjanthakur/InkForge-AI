250faf32237a282c5cfb5381e4aeee54928011ad
surjanthakur <tsurjan506@gmail.com>
2026-03-30 10:57:06 +0530

    s

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const { isCurrentUser } = useAuthContext();
   const authItems = ["Dashboard", "Features", "About", "Editor"];
-  const guestItems = ["Features", "About US", "Login", "Signup"];
+  const guestItems = ["Features", "About", "Login", "Signup"];
 
   const navItems = isCurrentUser ? authItems : guestItems;
 
