bad3b542b4a3ec86aaf43ca1ff13121a24524e15
surjanthakur <tsurjan506@gmail.com>
2026-04-01 22:16:03 +0530

    s

--- backend/src/router/users_router.py
+++ backend/src/router/users_router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, Depends, status, Response, Cookie
-from sqlmodel import desc
-from ..db.db_connection import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ..db.models import User
+from ..db.db_connection import get_session
 from ..schemas.user import UserCreate, LoginRequest, currentUserResponse
 from ..service.users_service import (
     create_user,
@@ -17,12 +16,7 @@ user_router = APIRouter(prefix="/users", tags=["Authentication routes"])
 
 
 # create accounts
-@user_router.post(
-    "/signup",
-    status_code=status.HTTP_201_CREATED,
-    description="Create a new user account.",
-    response_description="Returns the created user account information.",
-)
+@user_router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def create_account(
     req_form: UserCreate, session_db: AsyncSession = Depends(get_session)
 ):
@@ -30,12 +24,7 @@ async def create_account(
 
 
 # login accounts
-@user_router.post(
-    "/login",
-    status_code=status.HTTP_200_OK,
-    description="Authenticates a user with their email and password. If successful, creates a session and returns authentication cookies.",
-    response_description="Session is established and the user is authenticated.",
-)
+@user_router.post("/login", status_code=status.HTTP_200_OK)
 async def login_account(
     req_form: LoginRequest,
     response: Response,
@@ -49,24 +38,12 @@ async def login_account(
     "/me",
     status_code=status.HTTP_200_OK,
     response_model=currentUserResponse,
-    description="Get information about the currently authenticated user.",
-    response_description="Returns the currently authenticated user's profile, including their username, email, posts, and account creation date.",
 )
 async def get_current_user(curr_user: User = Depends(current_user)):
     return curr_user
 
 
 # logout accounts
-@user_router.post(
-    "/logout",
-    status_code=status.HTTP_200_OK,
-    description="Logs out the currently authenticated user by ending their session.",
-    response_description="User session terminated and authentication cookies revoked.",
-)
+@user_router.post("/logout", status_code=status.HTTP_200_OK)
 async def logout_account(res: Response, session_id: str = Cookie(None)):
     return await logout_user(session_id=session_id, response=res)
-
-
-@user_router.post("/auth/google")
-def google_auth(data: dict):
-    pass
