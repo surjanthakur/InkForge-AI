c3e6140c57b3d135f1804ee6eae3ac33ac9f08b7
surjanthakur <tsurjan506@gmail.com>
2026-04-04 00:08:02 +0530

    save

---
+++ backend/src/service/google_auth.py
@@ -0,0 +1,24 @@
+from fastapi import status, HTTPException
+from fastapi.responses import RedirectResponse
+from authlib.integrations.starlette_client import OAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+google_client_id = os.getenv("GOOGLE_CLIENT_ID")
+google_client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
+
+oauth = OAuth()
+
+oauth.register(
+    name="google",
+    client_id=google_client_id,
+    client_secret=google_client_secret,
+    authorize_url="https://accounts.google.com/o/oauth2/auth",
+    access_token_url="https://oauth2.googleapis.com/token",
+)
+
+
+async def google_callback():
+    pass
